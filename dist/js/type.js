1c672dccf9735f9deb92083da9412fccf5bd5d9e
Black-Hawk22 <58360668+Black-Hawk22@users.noreply.github.com>
2020-01-04 14:00:02 +0530

    Final

---
+++ dist/js/type.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".welcome", {
+    strings: [" a Web Developer ", " an Android Developer ", " a Programmer " , " a Poet " ],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true
+  });
